deeffb46da82c4abe85d52fbd003e16b1c68de72
김현 <jun123101@gmail.com>
2017-07-29 21:10:15 +0900

    학번입력, Start 누르면 node.js 서버에서 desk 값 받아와서 Button 값 On, Off 설정

--- app/src/main/java/com/example/kimhyun/desk_control/ClassActivity.java
+++ app/src/main/java/com/example/kimhyun/desk_control/ClassActivity.java
@@ -22,7 +22,7 @@ import java.net.URLEncoder;
 
 public class ClassActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btn1, btn2, btn3, btn4;
+    Button [] btn = new Button[4];
 
     private String html = "";
     private Handler mHandler;
@@ -33,31 +33,42 @@ public class ClassActivity extends AppCompatActivity implements View.OnClickList
     private BufferedReader networkReader;
     private BufferedWriter networkWriter;
 
-    private String ip = "192.9.44.159"; // IP
-    private int port = 1234; // PORT번호
+    private String ip = "192.168.1.4"; // IP
+    private int port = 3000; // PORT번호
+
+    String mJsonString;
+
+    String naverHtml;
 
     String student_num;
+
+    String [] desk = new String[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
+        btn[0] = (Button) findViewById(R.id.btn1);
+        btn[1] = (Button) findViewById(R.id.btn2);
+        btn[2] = (Button) findViewById(R.id.btn3);
+        btn[3] = (Button) findViewById(R.id.btn4);
 
-        btn1.setOnClickListener(this);
+        btn[0].setOnClickListener(this);
+        btn[1].setOnClickListener(this);
+        btn[2].setOnClickListener(this);
+        btn[3].setOnClickListener(this);
 
         Intent intent = getIntent();
         student_num = intent.getStringExtra("number").toString();
-        Log.d("11111", student_num);
+
+        Log.d("start", "start");
 
         // Thread로 웹서버에 접속
         new Thread() {
             public void run() {
-                String naverHtml = getNaverHtml();
+                Log.d("Thread", "Thread");
+                naverHtml = getNaverHtml();
 
                 Bundle bun = new Bundle();
                 bun.putString("NAVER_HTML", naverHtml);
@@ -72,8 +83,10 @@ public class ClassActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private String getNaverHtml(){
-        String naverHtml = "";
+        naverHtml = "";
 
+
+        Log.d("getNaverHtml", "getNaverHtml");
         URL url =null;
         HttpURLConnection http = null;
         InputStreamReader isr = null;
@@ -85,13 +98,19 @@ public class ClassActivity extends AppCompatActivity implements View.OnClickList
             http.setConnectTimeout(3*1000);
             http.setReadTimeout(3*1000);
 
+            Log.d("try", "try");
             isr = new InputStreamReader(http.getInputStream());
             br = new BufferedReader(isr);
 
             String str = null;
             while ((str = br.readLine()) != null) {
                 naverHtml += str + "\n";
+                Log.d("naver", naverHtml);
             }
+
+            Log.d("2222", "2222");
+
+            showResult();
 
         }catch(Exception e){
             Log.e("Exception", e.toString());
@@ -120,6 +139,33 @@ public class ClassActivity extends AppCompatActivity implements View.OnClickList
         }
     };
 
+
+    private void showResult() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        for(int i = 0; i < 4; i++) {
+                            Log.d("for문", String.valueOf(i));
+                            desk[i] = String.valueOf(naverHtml.charAt(i+11));
+                            if (desk[i].equals("0"))
+                                btn[i].setText("off");
+                            else
+                                btn[i].setText("on");
+                        }
+                    }
+                });
+            }
+        }).start();
+
+
+    }
+
+
     @Override
     public void onClick(View v) {
 
@@ -141,33 +187,41 @@ public class ClassActivity extends AppCompatActivity implements View.OnClickList
         BufferedReader br = null;
 
         try{
-            url = new URL("http://" + ip + ":" + port);
+            url = new URL("http://192.168.1.4:3000");
+            //url = new URL("http://jun123101.cafe24.com");
+            Log.d("post", "start");
             http = (HttpURLConnection) url.openConnection();
+            Log.d("post", "connect");
             //start
             String data = URLEncoder.encode("student_num", "UTF-8") + "="
                     + URLEncoder.encode(student_num, "UTF-8");
             data += "&" + URLEncoder.encode("table_num", "UTF-8") + "="
                     + URLEncoder.encode("1", "UTF-8");
 
+            Log.d("post", data);
             URLConnection conn = url.openConnection();
 
             conn.setDoOutput(true);
             OutputStreamWriter wr =
                     new OutputStreamWriter(conn.getOutputStream());
 
-            wr.write(data); //현재 위도, 경도값을 data에 넣고 near.php에 보낸다.
+            Log.d("post", "write전");
+            wr.write(data);
             wr.flush();
+            Log.d("post", "write후");
 
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(conn.getInputStream()));
 
             StringBuilder sb = new StringBuilder();
             String line;
+            Log.d("post", "00000");
 
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
             }
 
+            Log.d("post", "11111");
 
             reader.close();
 
@@ -215,5 +269,129 @@ public class ClassActivity extends AppCompatActivity implements View.OnClickList
 
         return naverHtml;
     }
+//
+//    public String sendHttpWithMsg(String url){
+//
+////기본적인 설정
+//
+//        DefaultHttpClient client = new DefaultHttpClient();
+//
+//        HttpPost post = new HttpPost(url);
+//
+//        HttpParams params = client.getParams();
+//
+//        HttpConnectionParams.setConnectionTimeout(params, 3000);
+//
+//        HttpConnectionParams.setSoTimeout(params, 3000);
+//
+//        post.setHeader("Content-type", "application/json; charset=utf-8");
+//
+//
+//
+//// JSON OBject를 생성하고 데이터를 입력합니다.
+//
+////여기서 처음에 봤던 데이터가 만들어집니다.
+//
+//        JSONObject jObj = new JSONObject();
+//
+//        try {
+//
+//            jObj.put("name", "hong");
+//
+//            jObj.put("phone", "000-0000");
+//
+//
+//
+//        } catch (JSONException e1) {
+//
+//            e1.printStackTrace();
+//
+//        }
+//
+//
+//
+//
+//
+//
+//
+//        try {
+//
+//// JSON을 String 형변환하여 httpEntity에 넣어줍니다.
+//
+//            StringEntity se;
+//
+//            se = new StringEntity(jObj.toString());
+//
+//            HttpEntity he=se;
+//
+//            post.setEntity(he);
+//
+//
+//
+//        } catch (UnsupportedEncodingException e1) {
+//
+//            e1.printStackTrace();	}
+//
+//
+//
+//
+//
+//        try {
+//
+////httpPost 를 서버로 보내고 응답을 받습니다.
+//
+//            HttpResponse response = client.execute(post);
+//
+//// 받아온 응답으로부터 내용을 받아옵니다.
+//
+//// 단순한 string으로 읽어와 그내용을 리턴해줍니다.
+//
+//            BufferedReader bufReader =
+//
+//                    new BufferedReader(new InputStreamReader(
+//
+//                            response.getEntity().getContent(),
+//
+//                            "utf-8"
+//
+//                    )
+//
+//                    );
+//
+//
+//
+//            String line = null;
+//
+//            String result = "";
+//
+//
+//
+//            while ((line = bufReader.readLine())!=null){
+//
+//                result +=line;
+//
+//            }
+//
+//            return result;
+//
+//
+//
+//        } catch (ClientProtocolException e) {
+//
+//            e.printStackTrace();
+//
+//            return "Error"+e.toString();
+//
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//
+//            return "Error"+e.toString();
+//
+//        }
+//
+//    }
+//
+
 
 }
